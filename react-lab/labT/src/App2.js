e4ff54f4c56b358ab284aef818ff49d4f5d528ae
Mark Zhitomirski <marcuzero@gmail.com>
2025-07-04 00:20:00 +0400

    assets/ <img > example

---
+++ react-lab/labT/src/App2.js
@@ -0,0 +1,13 @@
+import car from "./assets/images/car.jpg";
+
+function CarImage() {
+   return ( 
+      <img 
+        height={200}
+        src={car}
+        alt="Car image" 
+      />
+   );
+};
+
+export default CarImage;
